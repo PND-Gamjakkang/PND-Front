d5ab71804ec1cdde0b3d4737d14247d0ae6b9193
Jun Young <aa2030822@naver.com>
2024-07-24 20:55:36 +0900

    :feat: 내용 수정

--- src/App.js
+++ src/App.js
@@ -1,76 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { styled, ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './style/globalStyle';
-import { theme } from './style/theme.js';
-import { Outlet, useLocation } from 'react-router-dom';
-
-import Header from './components/header/Header.jsx';
-
-// css 초기설정
-const BackGroundColor = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  background-color: black;
-  position: relative;
-
-  color:white;
-`;
-
-const Content = styled.div`
-  flex-grow: 1;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 0;
-`;
-
-const Layout = () => {
-  const location = useLocation();
-
-  const currentPath = location.pathname;
-
-  // 인트로, 로그인, 회원가입 시 푸터 숨김
-  // const hideFooter =
-  //   currentPath === '/Signin' ||
-  //   currentPath === '/SignUp' ||
-  //   currentPath === '/';
-
-  return (
-    <BackGroundColor>
-        <Header />
-        <Content>
-          <Outlet />
-        </Content>
-        {/* {!hideFooter && <Footer />} */}
-    </BackGroundColor>
-  );
-};
-
-function App() {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout />
-      </ThemeProvider>
-    </>
-  );
-}
-=======
-import logo from './logo.svg';
-import './App.css';
-=======
-import "./App.css"
-import { useState } from "react";
-import { useRef } from "react";
-import Header from "./layout_test/Header";
-import InputAreaHeader from "./layout_test/InputAreaHeader";
-import InputArea from "./layout_test/Texts/InputArea";
-import MdArea from "./layout_test/Texts/MdArea";
-
-
+import React, { useState, useRef } from 'react';
+import Header from './layout_test/Header';
+import InputAreaHeader from './layout_test/InputAreaHeader';
+import MdArea from './layout_test/Texts/MdArea'
+import InputArea from './layout_test/Texts/InputArea'
+import './App.css'
 function App(){
   //inputArea Ref
   const inputRef = useRef(null); 
@@ -97,7 +30,6 @@ function App(){
     }
     setSelectedText("");
   };
->>>>>>> 983af69 (:memo: README에디터 초안)
 
   return (
     <div className="App">
@@ -119,12 +51,6 @@ function App(){
     </div>
   );
 
-<<<<<<< HEAD
->>>>>>> 4d2f362 (Initialize project using Create React App)
-export default App;
-=======
-
 };
-
 export default App;
->>>>>>> 983af69 (:memo: README에디터 초안)
+
