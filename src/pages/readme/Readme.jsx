dbae614463e1f112dc8944be7c3870558642589e
binniejung <ybe1027@naver.com>
2024-07-25 00:29:14 +0900

    ✅ Push Test

--- src/pages/readme/Readme.jsx
+++ src/pages/readme/Readme.jsx
@@ -5,6 +5,7 @@ function ReadMe() {
     return (
         <S.ReadmeLayout>
             <S.Title>리드미화면입니다</S.Title>
+            <S.ProfileBox></S.ProfileBox>
 
         </S.ReadmeLayout>
     )
--- src/pages/readme/ReadmeStyle.jsx
+++ src/pages/readme/ReadmeStyle.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ReadmeLayout = styled.div`
 display:flex;
+flex-direction:column;
 align-items: center;
 justify-content:center;
 
@@ -9,4 +10,11 @@ justify-content:center;
 
 export const Title = styled.div`
 
+`;
+
+export const ProfileBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #3498db;
+  border-radius: 50%;
 `;
