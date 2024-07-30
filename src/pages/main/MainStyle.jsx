9a872c9d28d05533b454bac33cb959821195c0fd
binniejung <ybe1027@naver.com>
2024-07-31 00:01:17 +0900

    💄 반응형으로 css 단위 모두 수정

--- src/pages/main/MainStyle.jsx
+++ src/pages/main/MainStyle.jsx
@@ -3,13 +3,11 @@ import styled from "styled-components";
 export const MainLayout = styled.div`
 width: 100%;
 display:flex;
-
 `;
 export const MainLeft = styled.div`
-
 display:flex;
 flex-direction: column;
-padding: 137px 137px 27px 137px;
+padding: 5vw 7vw 2vw 7vw;
 `;
 
 export const MainRight = styled.div`
@@ -19,7 +17,7 @@ export const MainRight = styled.div`
 export const MainTextTop = styled.div`
 text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 font-family: Inter;
-font-size: 60px;
+font-size: 4vw;
 font-style: italic;
 font-weight: 700;
 line-height: 164%; /* 98.4px */
@@ -27,9 +25,9 @@ line-height: 164%; /* 98.4px */
 
 export const MainTextBottom = styled.div` 
 font-family: Inter;
-font-size: 20px;
+font-size: 1.5vw;
 line-height: 150%; /* 30px */ 
-margin-top : 30px;
+margin-top : 1.5vw;
 
 `;
 
@@ -39,23 +37,24 @@ font-style: italic;
 `;
 
 export const MainButton = styled.button`
-width: 560px;
-height: 98px;
+width: 38vw;
+height: 5.1vw;
 flex-shrink: 0;
 background-color: ${({ theme }) => theme.colors.white};
 color: ${({ theme }) => theme.colors.black};
 border-radius: 50px;
 font-family: Inter;
-font-size: 34px;
+font-size: 2vw;
 font-style: normal;
 font-weight: 600;
 line-height: 150%; /* 51px */
+margin-top: 1.5vw;
 `;
 
 export const MainFeatures = styled.div`
 display: flex;
-gap: 98px;
-margin-top: 30px;
+gap: 6.1vw;
+margin-top: 1.5vw;
 `;
 
 export const FeatureBox = styled.div`
@@ -63,6 +62,7 @@ export const FeatureBox = styled.div`
 `;
 
 export const FeatureImg = styled.img`
+width: 10vw;
 `;
 
 export const FeatureText = styled.div`
@@ -75,12 +75,14 @@ line-height: 175%; /* 28px */
 `;
 
 export const MainRightImg = styled.img`
+width: 100%;
+height: 60vw;
 `;
 
 export const LinkToTeamPage = styled.div`
 display: inline-block; /* 글자에 맞게 선이 그어지도록 */
 flex-shrink: 0;
 color: ${({ theme }) => theme.colors.white};
-margin-top: 153px;
+margin-top: 8vw;
 border-bottom: 2px solid ${({ theme }) => theme.colors.white};
 `;
